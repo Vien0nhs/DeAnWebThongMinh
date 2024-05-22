815722755d44b94b09d99f3e04f248229c25192c
Hoangviet24 <hoangvietdoican2004@gmail.com>
2024-05-22 23:51:47 +0700

    Here

--- DeAnWebThongMinh/obj/Debug/net8.0/DeAnWebThongMinh.AssemblyInfo.cs
+++ DeAnWebThongMinh/obj/Debug/net8.0/DeAnWebThongMinh.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeAnWebThongMinh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b57a6fbb89e820a2c82121ef3f88a62bdfcc623")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f2e087dc9476a848695c677b493cd29b7b7d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeAnWebThongMinh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeAnWebThongMinh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
